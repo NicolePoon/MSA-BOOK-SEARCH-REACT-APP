ae31b56811fa58b579559eb8afe32015e882f8d2
NicolePoon <qpan344@acuklanduni.ac.nz>
2020-07-23 15:23:53 +1200

    Change default search to joy

--- book-app/src/App.tsx
+++ book-app/src/App.tsx
@@ -19,7 +19,7 @@ const theme = createMuiTheme({
 
 function App() {
   const [UserInput, setUserInput] = useState<IUserInput>({
-    SearchQuery: "smile",
+    SearchQuery: "joy",
   });
   function SetUserInput(a: IUserInput) {
     setUserInput(a);    
