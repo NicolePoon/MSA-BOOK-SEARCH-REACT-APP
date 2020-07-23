c3fb7a9df4ce58b06ddcd07dc544a4c3482045c5
NicolePoon <qpan344@acuklanduni.ac.nz>
2020-07-23 20:45:13 +1200

    set default search to smile

--- book-app/src/App.tsx
+++ book-app/src/App.tsx
@@ -19,7 +19,7 @@ const theme = createMuiTheme({
 
 function App() {
   const [UserInput, setUserInput] = useState<IUserInput>({
-    SearchQuery: "animal",
+    SearchQuery: "smile",
   });
   function SetUserInput(a: IUserInput) {
     setUserInput(a);    
